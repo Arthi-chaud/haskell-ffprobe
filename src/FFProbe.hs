87e3c78a97d0870efc300c4f84a1391fbaee6cb7
Arthur Jamet <arthur.jamet@gmail.com>
2024-06-29 18:13:35 +0200

    Add FFProbeData + entrypoint function

--- src/FFProbe.hs
+++ src/FFProbe.hs
@@ -2,7 +2,22 @@ module FFProbe (
     ffprobe,
 ) where
 
+import Data.Aeson (FromJSON, eitherDecodeStrict)
+import Data.ByteString.Char8 (pack)
+import FFProbe.Data.Chapter (Chapter)
 import FFProbe.Exec (execFFProbe)
+import GHC.Generics (Generic)
 
-ffprobe :: String -> IO (Either String String)
-ffprobe path = execFFProbe path
+data FFProbeData = FFProbeData
+    { chapters :: [Chapter]
+    }
+    deriving (Generic)
+
+instance FromJSON FFProbeData
+
+ffprobe :: String -> IO (Either String FFProbeData)
+ffprobe path = do
+    probeRes <- execFFProbe path
+    case probeRes of
+        Right rawJson -> return $ eitherDecodeStrict (pack rawJson)
+        Left err -> return $ Left err
